4ff6b64c282036ace6d29b7dea25608946f05bde
Pink <ndc2981996@gmail.com>
2018-12-26 21:32:25 +0700

    Add GET/:id in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ let {mongoose} = require('./db/mongoose');
 // let mongoose = require('./db/mongoose').mongoose
 let {Todos} = require('./models/Todos'); 
 let {User} = require('./models/User');
+let {ObjectID} = require('mongodb'); 
 
 let express = require('express'); 
 let bodyParser = require('body-parser'); 
@@ -28,7 +29,20 @@ app.get('/Todos', (req, res) => {
   }) 
 } , (err) => { 
   res.send(err);     
-}); 
+});  
+
+app.get('/Todos/:id', (req, res) => { 
+  let id = req.params.id;
+  if(!ObjectID.isValid(id)) { 
+    return res.status(404).send();
+  } 
+  Todos.findById(id).then((todo) => { 
+    if(!todo){ 
+      return res.status(404).send();
+    } 
+      return res.send({todo});
+  }).catch((e) => {res.status(400).send()});
+});
 
 app.listen(3000, ()=> { 
 console.log('Started on port 3000');
